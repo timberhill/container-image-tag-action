62b55ba188af6b6aa7f02eeed95056b928283dd5
Maksym Lisogorskyi <m.lisogorskyi@gmail.com>
2022-09-08 21:52:10 +0100

    implemented the logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,10 @@ debug () {
 image_name=$1
 minor_pattern=$2
 major_pattern=$3
-prefix="v"
-if [$4 -eq "false"];
+prefix=""
+if [$4 -eq "true"];
 then
-  prefix=""
+  prefix="v"
 fi
 
 debug "Image name: $image_name"
@@ -18,34 +18,34 @@ debug "Minor pattern: $minor_pattern"
 debug "Major pattern: $major_pattern"
 debug "Prefix: $prefix"
 
-echo "::set-output name=image::$image_name"
-
-
-# get the latest image tags
-
+# get the latest image tag
 container_tags=$(curl -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" https://api.github.com/user/packages/container/container-image-tag-action/versions | jq -r '.[].metadata.container.tags' | grep -vE "\[|\]" | sed "s/\"//g" | sed "s/ //g")
 debug "Container tags: \n$container_tags"
 
-current_container_tag="$(semver $container_tags | tail -n 1)"
-debug "Last semver container tag: $current_container_tag"
+tag="$(semver $container_tags | sort | tail -n 1)"
+debug "Last semver container tag: $tag"
 
 # get git history to determine the bump level
-
-git fetch --tags
+git fetch --all
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 debug "Current branch is $current_branch"
 
-
-
-tagFmt="^v?[0-9]+\.[0-9]+\.[0-9]+$" 
-taglist="$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)' | grep -E "$tagFmt")"
-debug "Tag list $taglist"
-
-tag="$(semver $taglist | tail -n 1)"
-debug "Last semver tag is $tag"
-
-
-log=$(git log $prefix$tag..HEAD --pretty='%B')
-
+log=$(git log main..$current_branch --pretty='%B')
 debug "Git log: $log"
 
+# bump the version
+case "$log" in
+    *$major_pattern* )
+        new_tag=$(semver -i major $tag); part="major"
+    ;;
+    *$minor_pattern* )
+    new_tag=$(semver -i minor $tag); part="minor"
+    ;;
+    * )
+    new_tag=$(semver -i patch $tag); part="patch"
+    ;;
+esac
+
+debug "$part version bump: $tag -> $new_tag"
+
+echo "::set-output name=imagetag::$new_tag"
